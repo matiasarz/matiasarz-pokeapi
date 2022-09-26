4c673356fcbf98e236913aa63efab3a6997c6ca5
MatiasArzamendia <arzamendia.matias99@gmail.com>
2022-09-26 05:19:52 -0300

    secciones de buscar finalizadas

--- app.js
+++ app.js
@@ -1,133 +1,77 @@
 const inputSearch = document.getElementById('inputSearch');
 const mainContainer = document.querySelector('.mainContainer');
-// const inputLetter = document.getElementById("searchByLetter");
 
 const templateCardPokemon = document.getElementById('cardPokemon').content;
 const cardFragment = document.createDocumentFragment();
-// const showNumberResult = document.querySelector(".resultNumber");
-// const showLetterResult = document.querySelector(".resultLetter");
+
+const filters = document.querySelector('.typesPokemons');
 
 const cardContainer = document.getElementById('cardContainer');
 
 const form = document.getElementById('form');
 
-const quantity = document.getElementById('quantity');
-const name = document.getElementById('name');
-const type = document.getElementById('type');
-const all = document.getElementById('all');
 const navBar = document.getElementById('navBar');
 
-const getResults = async (number) => {
+const getAllPokemons = async (quantity) => {
     const response = await fetch(
-        `https://pokeapi.co/api/v2/pokemon?limit=${number}&offset=0`
+        `https://pokeapi.co/api/v2/pokemon?limit=${quantity}&offset=0`
     );
     const data = await response.json();
     return data.results;
 };
 
-const getUrl = async (url) => {
+const getPokemonsByUrl = async (url) => {
     const response = await fetch(url);
     const data = await response.json();
 
     return data;
 };
 
-// const resultSearchByNumber = async () => {
-//     const response = await fetch(
-//         `https://pokeapi.co/api/v2/pokemon?limit=${inputNumber.value}&offset=0`
-//     );
-//     const data = await response.json();
-//     const { results } = data;
-
-//     if (inputNumber.value !== "") {
-//         showNumberResult.textContent = "";
-//         for (let i = 0; i < inputNumber.value; i++) {
-//             showNumberResult.innerHTML += `<div><b>${i + 1}</b>- ${
-//                 results[i].name
-//             }</div>`;
-//         }
-//     } else {
-//         showNumberResult.textContent = "";
-//     }
-// };
-
-// const resultSearchByLetter = async () => {
-//     const response = await fetch(
-//         `https://pokeapi.co/api/v2/pokemon?limit=200&offset=0`
-//     );
-//     const data = await response.json();
-//     const { results } = data;
-
-//     showLetterResult.textContent = "";
-//     for (let i = 0; i < 200; i++) {
-//         if (inputLetter.value !== "") {
-//             if (results[i].name.startsWith(inputLetter.value)) {
-//                 showLetterResult.innerHTML += `<div>${results[i].name}</div>`;
-//             }
-//         } else {
-//             showLetterResult.textContent = "";
-//         }
-//     }
-// };
-
-// inputNumber.addEventListener("keyup", () => {
-//     if (!isNaN(inputNumber.value)) {
-//         showNumberResult.textContent = "";
-//         resultSearchByNumber();
-//     } else {
-//         showNumberResult.innerHTML = "<div>Ingrese un numero</div>";
-//     }
-// });
-// inputLetter.addEventListener("keyup", () => {
-//     if (isNaN(inputLetter.value)) {
-//         resultSearchByLetter();
-//     } else if (inputLetter.value === "") {
-//         showLetterResult.textContent = "";
-//     } else {
-//         showLetterResult.innerHTML = "<div>Ingrese una letra</div>";
-//     }
-// });
-
-const searchByQuantity = (url) => {
+const parameterUrl = ({ url }) => {
     const clone = templateCardPokemon.cloneNode(true);
-    cardFragment.textContent = '';
-    cardContainer.textContent = '';
-    if (!isNaN(inputSearch.value) && inputSearch.value !== '') {
-        getUrl(url).then((item) => {
-            let type = item.types.map((item) => item.type.name);
-            clone.querySelector('.card h1').innerText = item.name.toUpperCase();
-            clone.querySelector('.card img').src = item.sprites.front_default;
-            clone.querySelector('.card img').alt = item.name;
-            clone.querySelector('.card h4').innerText = type.join(' - ');
+    getPokemonsByUrl(url).then((item) => {
+        console.log(item);
+        let type = item.types.map((item) => item.type.name);
+        clone.querySelector('.card h1').innerText = item.name.toUpperCase();
+        clone.querySelector('.card img').src = item.sprites.front_default;
+        clone.querySelector('.card img').alt = item.name;
+        clone.querySelector('.card h4').innerText = type.join(' - ');
 
-            cardFragment.appendChild(clone);
-            cardContainer.appendChild(cardFragment);
-            mainContainer.appendChild(cardContainer);
-        });
-        return;
-    }
-    cardFragment.textContent = '';
-    cardContainer.textContent = '';
+        cardFragment.appendChild(clone);
+        cardContainer.appendChild(cardFragment);
+        mainContainer.appendChild(cardContainer);
+    });
 };
 
-const searchByName = (name, url) => {
+const filterTypes = ({ url, e }) => {
     const clone = templateCardPokemon.cloneNode(true);
+    getPokemonsByUrl(url).then((item) => {
+        item.types.filter((filtro) => {
+            if (filtro.type.name == e.target.id) {
+                clone.querySelector('.card h1').innerText = item.name;
+                clone.querySelector('.card img').src =
+                    item.sprites.front_default;
+                clone.querySelector('.card img').alt = item.name;
+                clone.querySelector('.card h4').innerText = filtro.type.name;
+
+                cardFragment.appendChild(clone);
+                cardContainer.appendChild(cardFragment);
+                mainContainer.appendChild(cardContainer);
+            }
+        });
+    });
+};
+
+const searchBy = ({ item, e }) => {
+    const { name, url } = item;
     cardFragment.textContent = '';
     cardContainer.textContent = '';
-    if (name.startsWith(inputSearch.value) && inputSearch.value !== '') {
-        getUrl(url).then((item) => {
-            let type = item.types.map((item) => item.type.name);
-            clone.querySelector('.card h1').innerText = item.name.toUpperCase();
-            clone.querySelector('.card img').src = item.sprites.front_default;
-            clone.querySelector('.card img').alt = item.name;
-            clone.querySelector('.card h4').innerText = type.join(' - ');
+    if (!isNaN(inputSearch.value) && inputSearch.value !== '')
+        parameterUrl({ url });
+    if (name.startsWith(inputSearch.value) && inputSearch.value !== '')
+        parameterUrl({ url });
 
-            cardFragment.appendChild(clone);
-            cardContainer.appendChild(cardFragment);
-            mainContainer.appendChild(cardContainer);
-        });
-        return;
-    }
+    if (e.target.checked) filterTypes({ url, e });
     cardFragment.textContent = '';
     cardContainer.textContent = '';
 };
@@ -135,45 +79,59 @@ const searchByName = (name, url) => {
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     if (inputSearch.placeholder === 'Buscar por cantidad') {
-        getResults(inputSearch.value).then((item) =>
-            item.forEach((item) => searchByQuantity(item.url))
+        getAllPokemons(inputSearch.value).then((item) =>
+            item.forEach((item) => searchBy({ item, e }))
         );
     } else if (inputSearch.placeholder === 'Buscar por nombre') {
-        getResults(10).then((item) =>
-            item.forEach((item) => searchByName(item.name, item.url))
+        getAllPokemons(10).then((item) =>
+            item.forEach((item) => searchBy({ item, e }))
         );
     }
 });
 
-navBar.addEventListener('click', (e) => {
-    inputSearch.placeholder = `${e.target.innerText}`;
-    inputSearch.value = '';
-    if (inputSearch.placeholder === 'Todos') {
-        mainContainer.textContent = '';
-        cardFragment.textContent = '';
-        cardContainer.textContent = '';
+navBar.querySelectorAll('ul li').forEach((item) =>
+    item.addEventListener('click', (e) => {
+        inputSearch.placeholder = `${e.target.innerText}`;
+        inputSearch.value = '';
+        if (inputSearch.placeholder === 'Todos') {
+            mainContainer.textContent = '';
+            cardFragment.textContent = '';
+            cardContainer.textContent = '';
 
-        getResults(5).then((item) => {
-            item.forEach((item) => {
-                getUrl(item.url).then((item) => {
-                    let type = item.types.map((item) => item.type.name);
-                    const clone = templateCardPokemon.cloneNode(true);
-                    clone.querySelector('.card h1').innerText =
-                        item.name.toUpperCase();
-                    clone.querySelector('.card img').src =
-                        item.sprites.front_default;
-                    clone.querySelector('.card img').alt = item.name;
-                    clone.querySelector('.card h4').innerText =
-                        type.join(' - ');
+            getAllPokemons(5).then((item) => {
+                item.forEach((item) => {
+                    getPokemonsByUrl(item.url).then((item) => {
+                        let type = item.types.map((item) => item.type.name);
+                        const clone = templateCardPokemon.cloneNode(true);
+                        clone.querySelector('.card h1').innerText =
+                            item.name.toUpperCase();
+                        clone.querySelector('.card img').src =
+                            item.sprites.front_default;
+                        clone.querySelector('.card img').alt = item.name;
+                        clone.querySelector('.card h4').innerText =
+                            type.join(' - ');
 
-                    cardFragment.appendChild(clone);
-                    cardContainer.appendChild(cardFragment);
-                    mainContainer.appendChild(cardContainer);
+                        cardFragment.appendChild(clone);
+                        cardContainer.appendChild(cardFragment);
+                        mainContainer.appendChild(cardContainer);
+                    });
                 });
             });
-        });
-    } else {
-        mainContainer.textContent = '';
-        mainContainer.appendChild(form);
-    }
-});
+        } else if (e.target.id === 'type') {
+            mainContainer.innerText = '';
+            mainContainer.appendChild(filters);
+            filters.style.display = 'flex';
+        } else {
+            mainContainer.textContent = '';
+            mainContainer.appendChild(form);
+        }
+    })
+);
+
+filters.querySelectorAll('div').forEach((item) =>
+    item.addEventListener('click', (e) => {
+        getAllPokemons(10).then((item) =>
+            item.forEach((item) => searchBy({ item, e }))
+        );
+    })
+);
